93f225802d48cdc133e2afeb2e64a76d1aa56d5c
Dusan Andrejevic <dule.andrejevic@gmail.com>
2021-08-08 23:22:25 +0200

    [App] setup app components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Header />
+    <Main />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from '@/components/Header.vue';
+import Main from '@/components/Main.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Main
   }
 }
 </script>
 
 <style>
-#app {
+@import url('./assets/css/reset.css');
+
+:root {
+  --primary: #44CE92;
+  --primaryText: #2C3E50;
+  --background: #E5E5E5;
+
+}
+
+.container {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: var(--primaryText);
 }
 </style>
