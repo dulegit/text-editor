dac47ef9d25d52abde2981cebdb51d43ab857473
Dusan Andrejevic <dule.andrejevic@gmail.com>
2021-08-08 23:22:46 +0200

    [Header] component created

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="header">
+    <h1 class="header__title">
+      Text Editor
+    </h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+<style scoped>
+.header {
+  padding: 1rem;
+  background: var(--primary);
+  color: #fff;
+  grid-column: 1/13;
+  grid-row: 1;
+}
+.header__title {
+  font-weight: 700;
+  font-size: 1.75rem;
+  text-align: center;
+}
+</style>
